1729d11417ab14f0c7b8658553ddd77390f18b95
Kabokalle <bpsvm4vgt7@privaterelay.appleid.com>
2026-02-26 19:52:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-const CACHE = "kallies-mini-v1";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ])
-    )
-  );
-});
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("SW registered"))
+    .catch(err => console.log("SW failed", err));
+}
